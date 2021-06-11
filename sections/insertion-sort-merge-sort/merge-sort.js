f0c0451188f451cab392a42beda1951ca0bd4ea9
danielgeri <danielgeri@gmail.com>
2021-06-11 15:58:50 -0700

    new probs for insertion and merge sort

---
+++ sections/insertion-sort-merge-sort/merge-sort.js
@@ -0,0 +1,51 @@
+Implement Merge Sort
+
+ 
+
+Problem Statement:
+
+You are given an array of integers. You have to sort the array using merge sort algorithm.
+
+Input/Output Format For The Function:
+
+Input Format:
+
+The function contains a single argument, an integer array arr.
+
+Output Format:
+
+Return the sorted integer array.
+
+Input/Output Format For The Custom Input:
+
+/*
+Input Format:
+
+First line contains integer n, the number of integers. The next n lines contains an integer each.
+
+If arr = [1,7,5,3] then input should be:
+
+4
+
+1
+
+7
+
+5
+
+3
+
+Output Format:
+
+Output the sorted array of integers, with each integer in a new line.
+
+For input arr = [1,7,5,3], output will be
+
+1
+
+3
+
+5
+
+7
+*/
